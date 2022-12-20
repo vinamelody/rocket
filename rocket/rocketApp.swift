b7ba1361ada0fed34bb75f10c6cb26ec99f764d3
Vina Melody <vina_melody@yahoo.com>
2022-12-20 09:30:23 +0800

    Initial Commit

---
+++ rocket/rocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  rocketApp.swift
+//  rocket
+//
+//  Created by Vina Melody on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct rocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rocketUITests/rocketUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rocketUITestsLaunchTests.swift
+//  rocketUITests
+//
+//  Created by Vina Melody on 20/12/22.
+//
+
+import XCTest
+
+final class rocketUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
